17e41d5bba544ba55dd8675e391ed12dc73ad8de
Jacob Jaeggli <jjaeggli@mailbox.org>
2024-09-14 10:50:44 -0600

    Create mb68xx python package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='mb68xx-utils',
+    version='0.1.0',
+    author='Jacob A. Jaeggli',
+    # author_email='',
+    packages=['mb68xx'],
+    # scripts=['bin/mb68xx-log.py'],
+    # url='http://pypi.python.org/pypi/TowelStuff/',
+    license='LICENSE',
+    description=('Utilities providing data logging and restart functionality for the Motorola '
+                 'MB6800 / MB6811 series cable modems'),
+    # long_description='',
+    install_requires=[],
+)
